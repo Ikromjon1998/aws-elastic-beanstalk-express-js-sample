eab21bf3645b032f0136c748780e3db7a23bfdc0
Ikromjon <ikromjon98.98@icloud.com>
2024-05-01 13:40:46 +0200

    change message

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World from Ikrom!'));
+app.get('/', (req, res) => res.send('Hello from Ikrom to everyone!'));
+
+// some other route to test with some dinamic content
+app.get('/test', (req, res) => {
+    // get the query parameter from the request
+    let name = req.query.name;
+    // send the response with the name
+    res.send(`Hello ${name}!`);
+});
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
